675a7f3373c005505bf77c4f4fa7fd20af9c07cf
ingarbi <ingarbi006@gmail.com>
2023-08-02 20:02:06 +0300

    3  deploy

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,8 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
-from .constants import PAGE_SIZE, MAX_PAGE_SIZE
+from .constants import MAX_PAGE_SIZE
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = PAGE_SIZE
     page_size_query_param = "limit"
     max_page_size = MAX_PAGE_SIZE
