1513e0f65bd2349d2bdb5cb0bfe742f9ed78899c
ingarbi <ingarbi006@gmail.com>
2023-08-03 12:45:59 +0300

    20 deploy

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -149,48 +149,52 @@ class RecipeIngredient(models.Model):
         )
 
 
-class FavouriteShoppingCart(models.Model):
+class Favourite(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='%(class)s',
-        verbose_name="Пользователь",
+        related_name='favorites',
+        verbose_name='Пользователь',
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='%(class)s',
-        verbose_name="Рецепт",
+        related_name='favorites',
+        verbose_name='Рецепт',
     )
 
     class Meta:
-        abstract = True
-
-
-class Favourites(FavouriteShoppingCart):
-
-    class Meta:
-        ordering = ("user", "recipe")
-        verbose_name = "Избранное"
-        verbose_name_plural = "Избранное"
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
         constraints = [
-            UniqueConstraint(fields=["user", "recipe"],
-                             name="unique_favourite")
+            UniqueConstraint(fields=['user', 'recipe'],
+                             name='unique_favourite')
         ]
 
     def __str__(self):
         return f'{self.user} добавил "{self.recipe}" в Избранное'
 
 
-class ShoppingCart(FavouriteShoppingCart):
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='shopping_cart',
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='shopping_cart',
+        verbose_name='Рецепт',
+    )
 
     class Meta:
-        ordering = ("user", "recipe")
-        verbose_name = "Корзина покупок"
-        verbose_name_plural = "Корзина покупок"
+        verbose_name = 'Корзина покупок'
+        verbose_name_plural = 'Корзина покупок'
         constraints = [
-            UniqueConstraint(fields=["user", "recipe"],
-                             name="unique_shopping_cart")
+            UniqueConstraint(fields=['user', 'recipe'],
+                             name='unique_shopping_cart')
         ]
 
     def __str__(self):
